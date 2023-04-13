69e920649857adfa7a4977bf3d653e755ea55870
Valorando <vadimsokil.dnipro@gmail.com>
2023-04-14 01:20:02 +0300

    Create a method for selecting a card type and entering personal data.

--- Source.cpp
+++ Source.cpp
@@ -58,6 +58,127 @@ public:
 		money = 0.0;
 
 	}
+
+	void Bank_system()
+	{
+		cout << "\t\t\tService login in progress..." << endl << endl;
+		cout << "\t\t\tWelcome to the PrivatBank customer service system." << endl;
+		cout << "\t\t\tYou don`t have a card yet, would you like to apply? (Yes - 1 or No - 2)" << endl;
+		while (!(cin >> key_one)) {
+			cout << endl;
+			cout << "\t\t\tError!";
+			cout << endl;
+			cin.clear();
+			cin.get();
+		}
+		if (key_one == 1)
+		{
+			cout << "\t\t\tSelect card type:" << endl;
+			cout << "\t\t\tDebit - 1" << endl;
+			cout << "\t\t\tCredit - 2" << endl;
+			cout << endl;
+			cout << "\t\t\tError!";
+			while (!(cin >> key_two)) {
+				cout << endl;
+				cout << "\t\t\tError!";
+				cout << endl;
+				cin.clear();
+				cin.get();
+			}
+			if (key_two == 1)
+			{
+				cout << "\t\t\tYou have selected the >Debit card< " << endl;
+				cout << "\t\t\tLet's start filling in the data." << endl;
+				cout << "\t\t\tEnter your full name\t";
+				cin >> name;
+				cout << endl;
+				cout << "\t\t\tEnter your full surname:\t";
+				cin >> surname;
+				cout << endl;
+				cout << "\t\t\tEnter your full patronymic:\t";
+				cin >> patr;
+				cout << endl;
+				cout << "\t\t\tEnter your day of birth:\t";
+				cin >> day;
+				cout << endl;
+				cout << "\t\t\tEnter your mounth of birth:\t";
+				cin >> mounth;
+				cout << endl;
+				cout << "\t\t\tEnter your year of birth:\t";
+				cin >> year;
+				cout << endl;
+				cout << "\t\t\tEnter your passport number:\t";
+				cin >> passport_number;
+				cout << endl;
+				cout << "\t\t\tEnter your phone number:\t";
+				cin >> phone_number;
+				cout << endl;
+				cout << "\t\t\tEnter password for your card:\t";
+				cin >> password_card;
+				cout << endl;
+				cout << "\t\t\tDone, check your data is correct." << endl;
+				cout << endl;
+				cout << "\t\t\tSNP: " << surname << " " << name << " " << patr << endl;
+				cout << "\t\t\tDate of birth: " << day << "/" << mounth << "/" << year << endl;
+				cout << "\t\t\tPassport number: " << passport_number << endl;
+				cout << "\t\t\tPhone number :" << phone_number << endl;
+				cout << "\t\t\tPassword of the card: " << password_card << endl;
+				cout << endl;
+			}
+
+			if (key_two == 2)
+			{
+				cout << "\t\t\tYou have selected the >Credit card< " << endl;
+				cout << "\t\t\tLet's start filling in the data." << endl;
+				cout << "\t\t\tEnter your full name\t";
+				cin >> name;
+				cout << endl;
+				cout << "\t\t\tEnter your full surname:\t";
+				cin >> surname;
+				cout << endl;
+				cout << "\t\t\tEnter your full patronymic:\t";
+				cin >> patr;
+				cout << endl;
+				cout << "\t\t\tEnter your day of birth:\t";
+				cin >> day;
+				cout << endl;
+				cout << "\t\t\tEnter your mounth of birth:\t";
+				cin >> mounth;
+				cout << endl;
+				cout << "\t\t\tEnter your year of birth:\t";
+				cin >> year;
+				cout << endl;
+				cout << "\t\t\tEnter your passport number:\t";
+				cin >> passport_number;
+				cout << endl;
+				cout << "\t\t\tEnter your phone number:\t";
+				cin >> phone_number;
+				cout << endl;
+				cout << "\t\t\tEnter password for your card:\t";
+				cin >> password_card;
+				cout << endl;
+				cout << "\t\t\tDone, check your data is correct." << endl;
+				cout << endl;
+				cout << "\t\t\tSNP: " << surname << " " << name << " " << patr << endl;
+				cout << endl;
+				cout << "\t\t\tDate of birth: " << day << "/" << mounth << "/" << year << endl;
+				cout << endl;
+				cout << "\t\t\tPassport number: " << passport_number << endl;
+				cout << endl;
+				cout << "\t\t\tPhone number :" << phone_number << endl;
+				cout << endl;
+				cout << "\t\t\tPassword of the card: " << password_card << endl;
+				cout << endl;
+				cout << endl;
+			}
+		}
+
+		if (key_one == 2)
+		{
+			exit(2);
+		}
+
+	}
 };
 
 
